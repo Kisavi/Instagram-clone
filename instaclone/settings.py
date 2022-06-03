01bfb59a9e23a7b6f5c0650475ace2008b45686c
Kisavi <deniskisavi@gmail.com>
2022-06-03 17:50:30 +0300

    add django default url path for user auth in my project urls

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -38,7 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'instagram',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_ALLOWED_TEMPLATES_PACKS = 'bootstrap5'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
