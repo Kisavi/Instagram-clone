cde0fccf56cde4481410726680f0cdb41f88c2e1
Kisavi <deniskisavi@gmail.com>
2022-06-03 17:33:56 +0300

    create views for different redirection functionality and displays

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .form import RegisterForm
+from django.contrib.auth import login, logout, authenticate
+
 
 # Create your views here.
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('/login')
+    else:
+        form = RegisterForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+
+def loginpage(request):
+    return render(request, 'registration/login.html')
+
+
+def home(request):
+    return render(request, 'main/home.html')
