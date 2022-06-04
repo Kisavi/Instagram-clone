5aee0c83b3bf6a84ea3882413bdd6e8c536f3b9b
Kisavi <deniskisavi@gmail.com>
2022-06-05 02:01:00 +0300

    create a profiles.css file

--- instagram/models.py
+++ instagram/models.py
@@ -21,7 +21,7 @@ class Post(models.Model):
     image = models.ImageField()
     caption = models.TextField(max_length=200)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    likes = models.ForeignKey(Likes, on_delete=models.SET_NULL)
+    pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.caption
