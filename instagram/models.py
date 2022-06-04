c7bb4c5227326e1febdfe3ec1ac5bdda89db8142
Kisavi <deniskisavi@gmail.com>
2022-06-04 23:08:29 +0300

    create models

--- instagram/models.py
+++ instagram/models.py
@@ -1,4 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
+# post *
+# like
+
+
+class Profile(models.Model):
+    bio = models.TextField(max_length=30)
+    profile_pic = models.ImageField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.bio
+
+
+class Post(models.Model):
+    image = models.ImageField()
+    caption = models.TextField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ForeignKey(Likes, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.caption
+
+
+class Comments(models.Model):
+    comment = models.TextField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.comment
+
+
+class Follow(models.Model):
+    followers = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
