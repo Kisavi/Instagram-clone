0764e8aa3d9fcccdc2e061334fe66a31245778c7
Kisavi <deniskisavi@gmail.com>
2022-06-05 03:36:18 +0300

    add template to display saved posts by user

--- instagram/urls.py
+++ instagram/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home),
+    path('', views.sign_up),
+    path('home', views.home, name='home'),
     path('sign-up', views.sign_up),
     path('profile', views.profile, name='profile'),
 ]
