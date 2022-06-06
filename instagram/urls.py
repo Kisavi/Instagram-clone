87520191d41d54c611a72ada05ee32044dd0f61c
Kisavi <deniskisavi@gmail.com>
2022-06-07 02:24:01 +0300

    create like route url and new signup url

--- instagram/urls.py
+++ instagram/urls.py
@@ -2,10 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.sign_up),
-    path('home', views.home, name='home'),
-    path('sign-up', views.sign_up),
-    path('profile/<str:username>', views.profile, name='profile'),
-    path('saved', views.saved, name='saved'),
-    path('post', views.new_post, name='post'),
+    path('home', views.index),
+    path('signup', views.sign_up),
+    path('', views.index, name='index'),
+    path('profile/<str:username>/', views.profile, name='profile'),
+    path('edit/profile/', views.update_profile, name='update'),
+    path('image/', views.post, name='post'),
+    path('like/<id>', views.like_post, name='like'),
+    path('search/', views.search_profile, name='search'),
+    path('user_profile/<username>/', views.user_profile, name='user_profile'),
+    path('unfollow/<to_unfollow>', views.unfollow, name='unfollow'),
+    path('follow/<to_follow>', views.follow, name='follow'),
+    path('image/<id>', views.comment, name='comment'),
 ]
