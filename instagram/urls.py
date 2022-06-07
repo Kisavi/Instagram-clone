b5965eee32e614a204ed24e867b6072fd2aba8ec
Kisavi <deniskisavi@gmail.com>
2022-06-07 03:41:20 +0300

    change the default load view to sign up

--- instagram/urls.py
+++ instagram/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('home', views.index),
     path('signup', views.sign_up),
-    path('', views.index, name='index'),
+    path('', views.sign_up, name='index'),
     path('profile/<str:username>/', views.profile, name='profile'),
     path('edit/profile/', views.update_profile, name='update'),
     path('image/', views.post, name='post'),
